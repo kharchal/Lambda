3ee840c14ffa5c20523416af58f84cebfc6640af
kharchal <hav@ukr.net>
2016-11-10 09:16:30 +0200

    L 01

---
+++ src/TryOut01.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Юля on 10.11.2016.
+ */
+public class TryOut01 {
+    public static void main(String[] args) {
+        Lamda lamda = n -> n + 1;
+        int n = lamda.process(5);
+        System.out.println(n);
+    }
+
+}
+
+interface Lamda {
+    int process(int n);
+}
