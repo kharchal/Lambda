ea5d972573e518add1c2fbcf77f6ff961b6c41df
Alex <hav@ukr.net>
2016-11-11 11:04:47 +0200

    111

--- src/Runner.java
+++ src/Runner.java
@@ -7,12 +7,14 @@ import java.util.function.Predicate;
  */
 public class Runner {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         eval(numbers, (n) -> true, "print all");
         eval(numbers, (n) -> false, "print none");
         eval(numbers, (n) -> n % 2 == 1, "print odd");
         eval(numbers, (n) -> n % 2 == 0, "print even");
-        eval(numbers, (n) -> n > 4, "print > 4");
+        eval(numbers, (n) -> n > 5, "print > 4");
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
     }
 
     private static void eval(List<Integer> numbers, Predicate<Integer> predicate, String str) {
